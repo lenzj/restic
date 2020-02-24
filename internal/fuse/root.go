a71bf1a3a32c1e5d3f564fcaabd93dfcea53617c
greatroar <@>
2020-02-24 23:35:41 +0100

    Fold fuse.MetaDir into fuse.Root
    MetaDir was introduced in 2c02efd1fe1d35239e0bbacabebc6c2ef9140cc4 to
    reduce code duplication, but since then the code has been altered so
    that it's only used in one place.

--- internal/fuse/root.go
+++ internal/fuse/root.go
@@ -3,12 +3,14 @@
 package fuse
 
 import (
+	"context"
 	"os"
 	"time"
 
 	"github.com/restic/restic/internal/debug"
 	"github.com/restic/restic/internal/restic"
 
+	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 )
 
@@ -30,7 +32,7 @@ type Root struct {
 
 	lastCheck time.Time
 
-	*MetaDir
+	entries map[string]fs.Node
 
 	uid, gid uint32
 }
@@ -59,20 +61,59 @@ func NewRoot(repo restic.Repository, cfg Config) *Root {
 		root.gid = uint32(os.Getgid())
 	}
 
-	entries := map[string]fs.Node{
+	root.entries = map[string]fs.Node{
 		"snapshots": NewSnapshotsDir(root, fs.GenerateDynamicInode(rootInode, "snapshots"), "", ""),
 		"tags":      NewTagsDir(root, fs.GenerateDynamicInode(rootInode, "tags")),
 		"hosts":     NewHostsDir(root, fs.GenerateDynamicInode(rootInode, "hosts")),
 		"ids":       NewSnapshotsIDSDir(root, fs.GenerateDynamicInode(rootInode, "ids")),
 	}
 
-	root.MetaDir = NewMetaDir(root, rootInode, entries)
-
 	return root
 }
+
+var _ = fs.HandleReadDirAller(&Root{})
+var _ = fs.NodeStringLookuper(&Root{})
 
 // Root is just there to satisfy fs.Root, it returns itself.
 func (r *Root) Root() (fs.Node, error) {
 	debug.Log("Root()")
 	return r, nil
+}
+
+// Attr returns the attributes for the root node.
+func (d *Root) Attr(ctx context.Context, attr *fuse.Attr) error {
+	attr.Inode = rootInode
+	attr.Mode = os.ModeDir | 0555
+	attr.Uid = d.uid
+	attr.Gid = d.gid
+
+	debug.Log("attr: %v", attr)
+	return nil
+}
+
+// ReadDirAll returns all entries directly below the root node.
+func (d *Root) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+	debug.Log("Root.ReadDirAll()")
+
+	items := make([]fuse.Dirent, 0, len(d.entries))
+	for name := range d.entries {
+		items = append(items, fuse.Dirent{
+			Inode: fs.GenerateDynamicInode(rootInode, name),
+			Name:  name,
+			Type:  fuse.DT_Dir,
+		})
+	}
+
+	return items, nil
+}
+
+// Lookup returns a specific entry from the root node.
+func (d *Root) Lookup(ctx context.Context, name string) (fs.Node, error) {
+	debug.Log("Root.Lookup(%s)", name)
+
+	if dir, ok := d.entries[name]; ok {
+		return dir, nil
+	}
+
+	return nil, fuse.ENOENT
 }
